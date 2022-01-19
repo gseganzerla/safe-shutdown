6393cbdfcaa3452bc5c6834e81db29acc784a28f
Guilherme Seganzerla <g.seganzerla@gmail.com>
2022-01-19 17:54:40 -0300

    feat: log test

--- safe_shutdown/services/log_service.py
+++ safe_shutdown/services/log_service.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 class LogService:
     def __init__(self) -> None:
-        logging.basicConfig(level=logging.INFO, filename=f"{Path.home()}/.safe_shutdown.log")
-        self.logger = logging.getLogger()
+        logging.basicConfig(level=logging.INFO, filename=f"{Path.home()}/.safe_shutdown.log", filemode='a')
+        self.logger = logging.getLogger('safe_shutdown')
 
     def write(self, object):
         self.logger.info(object)
