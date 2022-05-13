68d68915f202eb5051923c7f578623f33d4e27fc
gseganzerla <g.seganzerla@gmail.com>
2022-05-13 16:57:34 -0300

    fix: log path

--- safe_shutdown/services/log_service.py
+++ safe_shutdown/services/log_service.py
@@ -1,10 +1,17 @@
 import logging
 from pathlib import Path
 
+
 class LogService:
     def __init__(self) -> None:
-        logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S', level=logging.INFO, filename=f"{Path.home()}/.safe_shutdown.log", filemode='a')
-        self.logger = logging.getLogger('safe_shutdown')
+        logging.basicConfig(
+            format="%(asctime)s %(message)s",
+            datefmt="%Y/%m/%d %H:%M:%S",
+            level=logging.INFO,
+            filename="/var/log/safe-shutdown.log",
+            filemode="a",
+        )
+        self.logger = logging.getLogger("safe_shutdown")
 
     def write(self, object):
         self.logger.info(object)
